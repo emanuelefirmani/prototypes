63706f48253a20d9db72788d2fb9aec866720e87
Firmani Emanuele <FIRE0272@adgr.net>
2017-09-08 14:59:24 +0200

    HelloWorld can be dockerized

---
+++ IssuingService/DockerHelloWorld/build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+docker run -v /work/prototypes/IssuingService/src:/src mono nuget restore /src/Issuing.sln
+docker run -v /work/prototypes/IssuingService/src:/src mono xbuild /src/Issuing.sln
+
+docker build -t adunicorn/helloWorld .
