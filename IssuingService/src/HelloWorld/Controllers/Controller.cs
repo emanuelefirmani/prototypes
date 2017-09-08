5f5833614461d3f40368fde6bf4427deae9da05b
Firmani Emanuele <FIRE0272@adgr.net>
2017-09-08 14:18:51 +0200

    Simple HelloWorld returns guid and echo

---
+++ IssuingService/src/HelloWorld/Controllers/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Http;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloController: ApiController
+    {
+        [Route("api/hello/{message}")]
+        [HttpGet]
+        public string Get(string message)
+        {
+            var guid = Guid.NewGuid().ToString();
+            var returnedMessage = $"Hello, {message} '{guid}'";
+            Debug.WriteLine(returnedMessage);
+            return returnedMessage;
+        }
+
+    }
+}
---
+++ IssuingService/src/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Owin.Hosting;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string baseAddress = "http://localhost:10000/";
+            string baseAddress = "http://*:10000/";
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine("App started with address '{0}' on a machine '{1}' with OS '{2}'", baseAddress, Environment.MachineName, Environment.OSVersion);
+                Console.WriteLine();
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
